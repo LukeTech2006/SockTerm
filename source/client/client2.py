d04443f63641e24311a56bc00a277297277d3d9a
LukeTech2020 <lukaskretschmar1@gmail.com>
2023-05-25 16:03:21 +0200

    Update client2.py

--- source/client/client2.py
+++ source/client/client2.py
@@ -11,17 +11,8 @@ term = Terminal()
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 60000))
 
-print(term.home + term.clear + term.turquoise(term.center(r"""
-███████╗ ██████╗  ██████╗██╗  ██╗████████╗███████╗██████╗ ███╗   ███╗     ██╗    ██████╗ 
-██╔════╝██╔═══██╗██╔════╝██║ ██╔╝╚══██╔══╝██╔════╝██╔══██╗████╗ ████║    ███║   ██╔═████╗
-███████╗██║   ██║██║     █████╔╝    ██║   █████╗  ██████╔╝██╔████╔██║    ╚██║   ██║██╔██║
-╚════██║██║   ██║██║     ██╔═██╗    ██║   ██╔══╝  ██╔══██╗██║╚██╔╝██║     ██║   ████╔╝██║
-███████║╚██████╔╝╚██████╗██║  ██╗   ██║   ███████╗██║  ██║██║ ╚═╝ ██║     ██║██╗╚██████╔╝
-╚══════╝ ╚═════╝  ╚═════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝     ╚═╝╚═╝ ╚═════╝ 
-SockTerm - The Simple™ TCP Terminal
-""")))
-
 newinput =  []
+stop = threading.Event()
 input_queue = queue.Queue()
 response_queue = queue.Queue()
 curLine = ''
@@ -31,6 +22,7 @@ response_section = ""
 def input_thread():
     global curLine
     global newinput
+    global stop
     input_queue.put("TIME")
     
     while True:
@@ -45,12 +37,21 @@ def input_thread():
             elif inputtype == 3:
                 curLine = pwinput(term.move(term.height - 1, 0) + newinput[1]).upper()
                 input_queue.put(curLine)
+        if stop.is_set(): sys.exit(0)
 
 def print_thread():
     global newinput
-    global s
+    global s, stop
     response_section_height = term.height - 2  # Adjusted to leave space for user input
-    response_section = ''
+    response_section = term.home + term.clear + term.turquoise(term.center(r"""
+███████╗ ██████╗  ██████╗██╗  ██╗████████╗███████╗██████╗ ███╗   ███╗     ██╗    ██████╗ 
+██╔════╝██╔═══██╗██╔════╝██║ ██╔╝╚══██╔══╝██╔════╝██╔══██╗████╗ ████║    ███║   ██╔═████╗
+███████╗██║   ██║██║     █████╔╝    ██║   █████╗  ██████╔╝██╔████╔██║    ╚██║   ██║██╔██║
+╚════██║██║   ██║██║     ██╔═██╗    ██║   ██╔══╝  ██╔══██╗██║╚██╔╝██║     ██║   ████╔╝██║
+███████║╚██████╔╝╚██████╗██║  ██╗   ██║   ███████╗██║  ██║██║ ╚═╝ ██║     ██║██╗╚██████╔╝
+╚══════╝ ╚═════╝  ╚═════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝     ╚═╝╚═╝ ╚═════╝ 
+SockTerm - The Simple™ TCP Terminal
+""")) + "\n"
     recv = ''
 
     while True:
@@ -61,14 +62,15 @@ def print_thread():
 
             inputtype = 1
             if recv.count(EOF):
-                print(recv[:-4])
-                sys.exit(0)
+                response_section += recv[:-4]
+                stop.set()
             elif recv.count(ACK):
                 s.send(pickle.dumps("ACK"))
                 inputtype = 1
                 response_section += recv[:-4] + "\n"
             elif recv.count(NUI):
                 inputtype = 0
+                response_section += recv[:-4] + "\n"
             elif recv.count(CUI) > 0:
                 newinput = recv.split(CUI)
                 inputtype = 2
@@ -78,13 +80,17 @@ def print_thread():
                 inputtype = 3
                 response_section += newinput[0] + "\n"
 
+            if stop.is_set(): sys.exit(0)
             response_queue.put(inputtype)
+            
             # Check if the response section height is exceeded and reset if necessary
             if response_section.count('\n') > response_section_height:
                 response_section = response_section.split('\n', 1)[1]
 
             # Clear the terminal and print the response section and user input
             print(term.home + term.clear + response_section, end='')
+            
+            #Check for terminus
 
 input_thread = threading.Thread(target=input_thread)
 print_thread = threading.Thread(target=print_thread)
